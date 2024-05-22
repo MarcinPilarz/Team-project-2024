616f89d0d1929057be55cc0e6fd1a2e035483c75
MarcinPilarz <130612@student.uni.opole.pl>
2024-05-22 12:20:42 +0200

    Create basic backend environment
    Create basic project with springboot using spring initializr

---
+++ team-project-2024-src/backend/bookswap/src/test/java/com/springboot/bookswap/BookswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bookswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
